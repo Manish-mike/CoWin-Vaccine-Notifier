f5a95c3f08715b2adc3d1bf3e304d880b6ffce45
Manish-mike <85367888+Manish-mike@users.noreply.github.com>
2021-09-11 14:03:44 +0530

    Update CoWin-Vaccine-Notifier_script.py

--- CoWin-Vaccine-Notifier_script.py
+++ CoWin-Vaccine-Notifier_script.py
@@ -1,89 +1,75 @@
 import requests
+from pygame import mixer 
+from datetime import datetime, timedelta
 import time
-import mixer 
-from datetime import datetime,timedelta
 from plyer import notification
+import json
 
-user_age = 55
-pincodes=["110046","110058"]
-num_days = 6
+age = 21
+pincodes = ["110046" , "110058"]
+num_days = 7
 
-print("Starting Search for vaccine slots")
+print_flag = 'Y'
+
+print("Starting search for Covid vaccine slots!")
 
 actual = datetime.today()
 list_format = [actual + timedelta(days=i) for i in range(num_days)]
 actual_dates = [i.strftime("%d-%m-%Y") for i in list_format]
 
-def new_func(session):
-    availbleSlots = session['available_capacity']
-    return availbleSlots
+while True:
+    counter = 0   
 
-while(True) :
-    availbleCenters = 0
+    for pincode in pincodes:   
+        for given_date in actual_dates:
 
-    #We have to search for all pincodes for all the given dates
-    for pincode in pincodes:
-        for date in actual_dates:
-            #The API to hit for our operation
-            URL = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pincode}&date={date}"
-            header = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+            URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, given_date
 
-            result = requests.get(URL, headers=header)
-            #If the response is 200 then proceed
+            result = requests.get(URL)
+
             if result.ok:
                 response_json = result.json()
-                #We have to search for available capacity in each session for each center
-                for center in response_json['centers']:
-                    for session in center['sessions']:
-                        #If the condition is satisfied like the available capacity is not zero and age limit is also satisfied then print them
-                        if (session['available_capacity']>0 and session['min_age_limit']<=user_age and session["date"] == date ):
-                            availbleCenters = availbleCenters+1
-                            print('Center Name : ' , center['name'])
-                            print('Available slots : ' , session['available_capacity'])
-                            print('Pincode : ' , pincode)
-                            print('Vaccine Name : ' , session['vaccine'])
-                            print('Date : ', session['date'])
-                            print('----------------------------------')
-
-                            centerName = center['name']
-                            availbleSlots = new_func(session)
-                            dateOfSlot = session['date']
-                            vaccineName = session['vaccine']
-                            #plyer is used here to notify people in desktop.
-                            notification.notify(
-                                title="Vaccine Slots Availble",
-                                # the body of the notification
-                                message=f"Center Name : {centerName} \n Availble slots : {availbleSlots} \n Vaccine Name : {vaccineName} \n Date : {dateOfSlot}",
-                                #You can add a icon here also in the .ico format
-                                # the notification stays for 5sec
-                                timeout=5
-                            )
-
-
+                if response_json["centers"]:
+                    if(print_flag.lower() =='y'):
+                        for center in response_json["centers"]:
+                            for session in center["sessions"]:
+                                if (session["min_age_limit"] <= age and session["available_capacity"] > 0 ) :
+                                    print('Center Name : ' , center['name'])
+                                    print('Available slots : ' , session['available_capacity'])
+                                    print('Pincode : ' , pincode)
+                                    print('Vaccine Name : ' , session['vaccine'])
+                                    print('Date : ', session['date'])
+                                    print('Price : ', center['fee_type'])
+                                    print('----------------------------------')
+                                    centerName = center['name']
+                                    availbleSlots = session['available_capacity']
+                                    dateOfSlot = session['date']
+                                    vaccineName = session['vaccine']
+                                    price = center['fee_type']
+                                    notification.notify(
+                                    title="Vaccine Slots Availble",
+                                    # the body of the notification
+                                    message=f"Center Name : {centerName}, {pincode} \nAvailable slots : {availbleSlots}, {price}\nVaccine : {vaccineName}, Date : {dateOfSlot}",
+                                    app_icon = r"icon\icon.ico",
+                                    timeout=5
+                                    )
+                                    print("\n")
+                                    counter = counter + 1
             else:
-                print("No Response")
+                print("No Response!")
 
-    if availbleCenters==0:
-        print("No availble slots in these areas...")
-        notification.notify(
-            title="No Vaccine Slots are available",
-            # the body of the notification
-            message="Sorry, there is no available slot in your area",
-            # You can add a icon here also in the .ico format
-            # the notification stays for 5sec
-            timeout=5
-        )
-    else:
-        print(f"Hurray. Found {availbleCenters} results...")
-    #The process will resume after 300 seconds. That means we will make the APi call again after 5 minutes.
-    time.sleep(300)
-    print("Waited for 5 minutes. Start searching again")
-
-    if not counter:
-        print("No Vaccination slot available!")
-    else:
+    if counter:
         mixer.init()
-        mixer.music.load('sound/dingdong.wav')
+        mixer.music.load(r"sound\dingdong.wav")
         mixer.music.play()
         print("Search Completed!")
+    else:
+        mixer.init()
+        mixer.music.load(r"Notification Sounds.wav")
+        mixer.music.play()
+        print("No Vaccination slot available!")
+
+    dt = datetime.now() + timedelta(minutes=30)
+
+    while datetime.now() < dt:
+        time.sleep(10)
